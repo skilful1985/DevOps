7908c5f54ad61e8685a1d6707f4d0c10fbb6d198
Vladimir <vladimir@mylocal.ru>
2026-06-06 05:26:58 +0000

    Create HTML

--- lab_01/lab_01.sh
+++ lab_01/lab_01.sh
@@ -2,9 +2,23 @@
 
 #curl wttr.in/$1?format=j2 | jq '.["current_condition"][0] | .temp_C,.humidity' > /var/www/html/index.nginx-debian.html
 
+# Считываем значение с сайта
 temp=$(curl -s wttr.in/$1?format=j2 | jq -r '.["current_condition"][0] | .temp_C')
 humid=$(curl -s wttr.in/$1?format=j2 | jq -r '.["current_condition"][0] | .humidity')
 
-echo $1
-echo "Temperature,C =" $temp
-echo "Humidity =" $humid
+# Создаем файл HTML
+cat <<EOF > /var/www/html/index.nginx-debian.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My weather</title>
+</head>
+<body>
+    <h1>My city: $1</h1>
+    <ul>
+        <li>Temperature,C: $temp</li>
+        <li>Humidity: $humid</li>
+    </ul>
+</body>
+</html>
+EOF
