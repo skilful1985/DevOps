1a992a7ccaaf3dfe83fa401d7e793bf246df0e08
Vladimir <vladimir@mylocal.ru>
2026-06-06 05:11:57 +0000

    Update_01 Lab_01

--- lab_01/lab_01.sh
+++ lab_01/lab_01.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo $1
+#curl wttr.in/$1?format=j2 | jq '.["current_condition"][0] | .temp_C,.humidity' > /var/www/html/index.nginx-debian.html
 
-curl wttr.in/$1?format=j2 | jq '.["current_condition"][0] | .temp_C,.humidity' > /var/www/html/index.nginx-debian.html
+temp=$(curl -s wttr.in/$1?format=j2 | jq -r '.["current_condition"][0] | .temp_C')
+humid=$(curl -s wttr.in/$1?format=j2 | jq -r '.["current_condition"][0] | .humidity')
+
+echo $1
+echo "Temperature,C =" $temp
+echo "Humidity =" $humid
